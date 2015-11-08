3026229c4fa0ef78d4a92774a460b088fe9f7a78
Sandeep <sandeepravi.bits@gmail.com>
2015-11-08 23:53:44 +0000

    Added Comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,13 @@
+class Comment < ActiveRecord::Base
+
+  # Autocode: Relationships
+  belongs_to :post
+
+  # Autocode: Validations
+
+  # Autocode: Callback
+
+	# File Upload
+
+	# Soft Destroy
+end
