ec09493ff16bb6656a70bda7495cb75d42410cc1
Sandeep <sandeepravi.bits@gmail.com>
2015-11-08 23:54:21 +0000

    Modifying Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,15 @@
 Rails.application.routes.draw do
-
-  devise_for :users
 	# Engines
-  mount ApiTaster::Engine => "/api_taster"
-
-	# root
-
 	# Resources Start Here
-
-
 	# Api
+  devise_for :users, controllers: { registrations: 'api/v1/registrations',sessions: 'api/v1/sessions' ,passwords: 'api/v1/passwords'}
 
   namespace :api do
     namespace :v1 do
-	   resources :posts do
-   	end
-
-	
-	   resources :comments do
-   	end
-
-	
-
+     
     end
   end
 
-	# Devise routes start here
+  # Devise routes start here
 
 end
